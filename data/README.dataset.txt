0c42e77de3ffc3e2710ac8f1c4f9e4e9e99f14ff
Your Name <pond.fedfe.0l0@gmail.com>
2024-07-04 17:04:22 +0700

    first commit

---
+++ data/README.dataset.txt
@@ -0,0 +1,6 @@
+# Solar Panel  > 2024-07-03 3:54am
+https://universe.roboflow.com/solar-panel-damage-detectionsegmentation/solar-panel-o6dwf
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ plot.py
@@ -0,0 +1,16 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv(r"D:\machine_learning_AI_Builders\บท4\Segmentation\Instance_segmentation\runs\segment\train\results.csv")
+df.columns = df.columns.str.strip()
+
+plt.figure(figsize=(6,6))
+
+plt.plot(df["epoch"],df["train/cls_loss"],label="train/cls_loss",color = "blue",marker="x")
+plt.plot(df["epoch"],df["val/cls_loss"],label="val/cls_loss",color = "red",marker="x")
+
+plt.xlabel("Epoch")
+plt.ylabel("Loss")
+plt.legend()
+plt.title('Train and Validation Loss by Epoch')
+plt.show()
